8abfd67df4ecbc8868918701eb310350ab096870
sarannic <snickershelton11@gmail.com>
2018-07-10 17:29:47 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -18,9 +18,9 @@ end
 
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
-puts breakfast("French Toast")
-puts lunch("Sandwich")
-puts dinner("Steak")
+puts breakfast("pancakes")
+puts lunch("peanut butter")
+puts dinner("broccoli")
 
 ## This returns "Any time, is the right time for Sweedish fish!"
 
